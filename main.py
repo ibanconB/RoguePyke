85de3c18309b892200aeadf7c2db7dbfbb5e5d96
IVAN <igstrabajo2002@gmail.com>
2024-08-19 15:45:08 +0200

    adding procedural map creation

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
 
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
     map_width = 80
     map_height = 45
 
@@ -26,7 +30,14 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc,player}
 
-    game_map = generate_dungeon(map_width, map_height)
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
 
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
